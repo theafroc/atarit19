78bd7135dcca0aeb3413ac598c2d303bdc3df20a
Alexander Marinelli <ajmar36@student.monash.edu>
2019-09-13 19:28:00 +0800

    yarn

--- index.js
+++ index.js
@@ -1,64 +1,180 @@
 'use strict';
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const START_SEARCH_NO = 'START_SEARCH_NO';
+const START_SEARCH_YES = 'START_SEARCH_YES';
+const GREETING = 'GREETING';
+const AUSTRALIA_YES = 'AUSTRALIA_YES';
+const AU_LOC_PROVIDED = 'AU_LOC_PROVIDED';
+const PREFERENCE_PROVIDED = 'PREFERENCE_PROVIDED';
+const PREF_CLEANUP = 'PREF_CLEANUP';
+const PREF_REVEGETATION = 'PREF_REVEGETATION';
+const PREF_BIO_SURVEY = 'PREF_BIO_SURVEY';
+const PREF_CANVASSING = 'PREF_CANVASSING';
+const AUSTRALIA_NO = 'AUSTRALIA_NO';
+const OTHER_HELP_YES = 'OTHER_HELP_YES';
+const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
+const GOOGLE_GEOCODING_API = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+const MONGODB_URI = process.env.MONGODB_URI;
+const GOOGLE_GEOCODING_API_KEY = process.env.GOOGLE_GEOCODING_API_KEY;
 
-// Imports dependencies and set up http server
 const
+  request = require('request'),
   express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  body_parser = require('body-parser'),
+  mongoose = require('mongoose'),
+  app = express().use(body_parser.json()); // creates express http server
+
+ //var db = mongoose.connect(MONGODB_URI);
+ //var ChatStatus = require("./models/chatstatus");
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-    let body = req.body;
-  
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-  
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
-  
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-      });
-  
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
-  
-  });
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {
 
-  // Adds support for GET requests to our webhook
+  // Return a '200 OK' response to all events
+  res.status(200).send('EVENT_RECEIVED');
+
+  const body = req.body;
+
+  if (body.object === 'page') {
+      // Iterate over each entry
+      // There may be multiple if batched
+      if (body.entry && body.entry.length <= 0){
+        return;
+      }
+      body.entry.forEach((pageEntry) => {
+        // Iterate over each messaging event and handle accordingly
+        pageEntry.messaging.forEach((messagingEvent) => {
+          console.log({messagingEvent});
+          if (messagingEvent.postback) {
+            handlePostback(messagingEvent.sender.id, messagingEvent.postback);
+          } else if (messagingEvent.message) {
+            if (messagingEvent.message.quick_reply){
+              handlePostback(messagingEvent.sender.id, messagingEvent.message.quick_reply);
+            } else{
+              handleMessage(messagingEvent.sender.id, messagingEvent.message);
+            }
+          } else {
+            console.log(
+              'Webhook received unknown messagingEvent: ',
+              messagingEvent
+            );
+          }
+        });
+      });
+    }
+});
+
+// Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
+
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+function handleMessage(sender_psid, message) {
+  // check if it is a location message
+  console.log('handleMEssage message:', JSON.stringify(message));
+
+  const locationAttachment = message && message.attachments && message.attachments.find(a => a.type === 'location');
+  const coordinates = locationAttachment && locationAttachment.payload && locationAttachment.payload.coordinates;
+
+  if (coordinates && !isNaN(coordinates.lat) && !isNaN(coordinates.long)){
+    handleMessageWithLocationCoordinates(sender_psid, coordinates.lat, coordinates.long);
+    return;
+  } else if (message.nlp && message.nlp.entities && message.nlp.entities.location && message.nlp.entities.location.find(g => g.confidence > 0.8 && g.suggested)){
+    const locationName = message.nlp.entities.location.find(loc => loc.confidence > 0.8 && loc.suggested);
+    if (locationName.value){
+      const locationNameEncoded = encodeURIComponent(locationName.value);
+      callGeocodingApi(locationNameEncoded, sender_psid, handleConfirmLocation);
+    }
+    return;
+  } else if (message.nlp && message.nlp.entities && message.nlp.entities.greetings && message.nlp.entities.greetings.find(g => g.confidence > 0.8 && g.value === 'true')){
+    handlePostback(sender_psid, {payload: GREETING});
+    return;
+  }
+}
+
+
+function handlePostback(sender_psid, received_postback) {
+  // Get the payload for the postback
+  const payload = received_postback.payload;
+
+  // Set the response and udpate db based on the postback payload
+  switch (payload){
+    case START_SEARCH_YES:
+      updateStatus(sender_psid, payload, handleStartSearchYesPostback);
+      break;
+    case START_SEARCH_NO:
+      updateStatus(sender_psid, payload, handleStartSearchNoPostback);
+      break;
+    case OTHER_HELP_YES:
+      updateStatus(sender_psid, payload, handleOtherHelpPostback);
+      break;
+    case AUSTRALIA_YES:
+      updateStatus(sender_psid, payload, handleAustraliaYesPostback);
+      break;
+    case AU_LOC_PROVIDED:
+      updateStatus(sender_psid, payload, askForActivityPreference);
+      break;
+    case GREETING:
+      updateStatus(sender_psid, payload, handleGreetingPostback);
+      break;
+    case PREF_CLEANUP:
+    case PREF_REVEGETATION:
+    case PREF_BIO_SURVEY:
+    case PREF_CANVASSING:
+      updatePreference(sender_psid, payload, handlePreferencePostback);
+      break;
+    default:
+      console.log('Cannot differentiate the payload type');
+  }
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  console.log('message to be sent: ', response);
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "url": `${FACEBOOK_GRAPH_API_BASE_URL}me/messages`,
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    console.log("Message Sent Response body:", body);
+    if (err) {
+      console.error("Unable to send message:", err);
     }
   });
+}
+
